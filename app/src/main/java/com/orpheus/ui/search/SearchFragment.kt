cec3ad3839bfbba51a3d93f794c94ba20be00f9f
DanielC2001 <daniel-carcamo1@hotmail.com>
2024-04-15 13:10:47 -0400

    Foundation of Orpheus built

--- app/src/main/java/com/orpheus/ui/search/SearchFragment.kt
+++ app/src/main/java/com/orpheus/ui/search/SearchFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.orpheus.R
 import com.orpheus.databinding.FragmentSearchBinding
+import com.orpheus.ui.playlists.PlaylistAdapter
+import com.orpheus.ui.playlists.PlaylistModel
 
 class SearchFragment : Fragment() {
 
@@ -22,16 +25,26 @@ class SearchFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(SearchViewModel::class.java)
-
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textSearch
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val root = binding.root
+        val gridView = root.findViewById<GridView>(R.id.browse_all_grid)
+        val playlistArrayList: ArrayList<PlaylistModel> = ArrayList()
+        val playlistNames: ArrayList<String> = arrayListOf("Pop","Hip-Hop","R&B","Rock","Jazz","Metal","Pop-Rock","Punk", "Alt-Rock","Rap")
+        for (name in playlistNames) {
+            playlistArrayList.add(PlaylistModel(name, R.drawable.candy_mkgee))
         }
+
+        val recentSearchPlaylist1 = root.findViewById<View>(R.id.recent_search_playlist_1)
+        val recentSearch1TextView = recentSearchPlaylist1.findViewById<TextView>(R.id.playlist_name)
+        recentSearch1TextView.text = getString(R.string.dummy_album_name_1)
+
+        val recentSearchPlaylist2 = root.findViewById<View>(R.id.recent_search_playlist_2)
+        val recentSearch2TextView = recentSearchPlaylist2.findViewById<TextView>(R.id.playlist_name)
+        recentSearch2TextView.text = getString(R.string.dummy_album_name_2)
+
+        gridView.adapter = PlaylistAdapter(requireContext(), playlistArrayList)
+
         return root
     }
 
